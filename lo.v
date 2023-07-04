87590c1e420f0dd7ed85796b829b502b2c30d693
4ov <next.mohammed.amr@gmail.com>
2023-07-04 16:43:07 +0300

    some hooks related commit

--- lo.v
+++ lo.v
@@ -1,21 +1,18 @@
 module lo
 
-
-//check every value in values and execlude the falsy ones
+// check every value in values and execlude the falsy ones
 pub fn filter[T](values []T, checker fn (T) bool) []T {
 	mut result := []T{}
-	for value in values{
-		if checker(value){
+	for value in values {
+		if checker(value) {
 			result << value
 		}
 	}
 
-	return  result
+	return result
 }
 
-
-
-//each (should be named map but it cannot be)
+// each (should be named map but it cannot be)
 pub fn each[F, T](values []F, checker fn (F) T) []T {
 	mut result := []T{}
 	for i in values {
@@ -25,22 +22,19 @@ pub fn each[F, T](values []F, checker fn (F) T) []T {
 	return result
 }
 
-
-pub fn every[T](values []T, checker fn(T)bool) bool{
-	for value in values{
-		if !checker(value){
+pub fn every[T](values []T, checker fn (T) bool) bool {
+	for value in values {
+		if !checker(value) {
 			return false
 		}
 	}
 	return true
 }
 
-pub fn some[T](values []T, checker fn(T)bool) bool{
+pub fn some[T](values []T, checker fn (T) bool) bool {
 	mut result := []bool{}
-	for value in values{
+	for value in values {
 		result << checker(value)
 	}
 	return true in result
 }
-
-
